68cccb79d76a95579a55e0184399ab11d83e3b3a
se <StudyExchange@163.com>
2017-06-10 00:41:25 +0800

    [se] Pass basic 1003

--- PatBasicLevelPractise/src/1003/__init__.py
+++ PatBasicLevelPractise/src/1003/__init__.py
@@ -1,39 +1,78 @@
-#我要通过！(20)
-
 #import
-import re
 
 #define function
-def rule1(str):
-    re.compile('A*[PAT]+', flags)
-    return True
-
-#input
-# inputStr = input()
-# inputCounter = int(inputStr)
-# 
-# inputList = []
-# for index in range(inputCounter):
-#     inputStr = input()
-#     inputList.append(inputStr)
+def rule(str):
+    status = 0
+    pn = 0
+    an = 0
+    tn = 0
+    pre = 0
+    mid = 0
+    post = 0
+    for i in range(len(str)):
+        if not(str[i] == 'P' or str[i] == 'A' or str[i] == 'T'):
+            break
+        if str[i] == 'P':
+            pn = pn + 1
+        if str[i] == 'A':
+            an = an + 1
+        if str[i] == 'T':
+            tn = tn + 1
+            
+        if status == 0:
+            if str[i] == 'A':
+                pre = pre + 1
+            elif str[i] == 'P':
+                status = 1
+                continue
+            else:
+                return False
+        if status == 1:
+            if str[i] == 'A':
+                mid = mid + 1
+            elif str[i] == 'T':
+                status = 2
+                continue
+            else:
+                return False
+        if status == 2:
+            if str[i] == 'A':
+                post = post + 1
+            else:
+                return False
+#    print((pre, mid, post))
+    if pn!=1 or an<1 or tn!=1:
+        return False
+    if pre*mid == post:
+        return True
+    else:
+        return False            
 
 #body
-inputList = [
-    'PAT',
-    'PAAT',
-    'AAPATAA',
-    'AAPAATAAAA',
-    'xPATx',
-    'PT',
-    'Whatever',
-    'APAAATAA']
-print(inputList)
-resultList = []
+# inputList = [
+#     'PAT',
+#     'PAAT',
+#     'AAPATAA',
+#     'AAPAATAAAA',
+#     'xPATx',
+#     'PT',
+#     'Whatever',
+#     'APAAATAA']
+n = input()
+li = []
+for i in range(int(n)):
+    li.append(input())
+#print(li)
 
-for i in range(len(inputList)):
-    resultList.append(rule1(inputList[i]))
+#main
+resultList = []
+for i in range(len(li)):
+    resultList.append(rule(li[i]))
 
 
 #output
 for i in range(len(resultList)):
-    print(resultList[i])
+    if resultList[i]:
+        print('YES')
+    else:
+        print('NO')
